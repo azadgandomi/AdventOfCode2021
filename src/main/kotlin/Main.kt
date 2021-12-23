dbfbe05af4c1519920db2ab8102ab5415619ac99
Azad <azad.gandomi@king.com>
2021-12-23 16:44:09 +0100

    day 23 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -48,13 +48,11 @@ data class State(val hallway: List<Char>, val rooms: List<List<Char>>)
 data class StateCost(val state: State, val cost: Int)
 
 fun main() {
+    val goals = "ABCD".toList()
     val roomIndex = listOf(2, 4, 6, 8)
-    val queue = PriorityQueue<StateCost> { x, y -> x.cost - y.cost
-//        (x.cost + x.state.hallway.sumOf { getMultiplier(it) } + x.state.rooms.sumOf {
-//            it.withIndex().sumOf { if (it.value != '.' && it.index != getCorrectRoomIndex(it.value)) 3 * getMultiplier(it.value) else 0 }
-//        }) - (y.cost + y.state.hallway.sumOf { getMultiplier(it) } + y.state.rooms.sumOf {
-//            it.withIndex().sumOf { if (it.value != '.' && it.index != getCorrectRoomIndex(it.value)) 3 * getMultiplier(it.value) else 0 }
-//        })
+    val queue = PriorityQueue<StateCost> { x, y ->
+        x.cost - y.cost
+
     }
     queue.add(
         StateCost(
@@ -62,8 +60,8 @@ fun main() {
                 "...........".toList(),
                 listOf(
                     listOf('A', 'C', 'B', 'A'),
-//                    listOf('D', 'C', 'B', 'A'),
-//                    listOf('D', 'B', 'A', 'C'),
+                    listOf('D', 'C', 'B', 'A'),
+                    listOf('D', 'B', 'A', 'C'),
                     listOf('D', 'D', 'B', 'C')
                 )
             ),
@@ -87,25 +85,30 @@ fun main() {
         for (room in s.rooms.withIndex()) {
             for (c in room.value.withIndex()) {
                 if (c.value != '.') {
-                    val m = getMultiplier(c.value)
-                    for (hi in s.hallway.indices) {
-                        if (hi !in roomIndex && s.hallway.slice(hi toward roomIndex[c.index]).all { it == '.' }) {
-                            val newHallway = s.hallway.toMutableList()
-                            newHallway[hi] = c.value
-                            val newRooms = s.rooms.toMutableList()
-                            val newRoom = room.value.toMutableList()
-                            newRoom[c.index] = '.'
-                            newRooms[room.index] = newRoom
-                            queue.add(
-                                StateCost(
-                                    State(
-                                        newHallway,
-                                        newRooms
-                                    ),
-                                    stateCost.cost + m * ((room.index + 1) + abs(hi - roomIndex[c.index]))
+                    if ((room.index until s.rooms.size).all { s.rooms[it][c.index] == goals[c.index] }) {
+                        continue
+                    }
+                    if (room.index == 0 || (0 until room.index).all { s.rooms[it][c.index] == '.' }) {
+                        val m = getMultiplier(c.value)
+                        for (hi in s.hallway.indices) {
+                            if (hi !in roomIndex && s.hallway.slice(hi toward roomIndex[c.index]).all { it == '.' }) {
+                                val newHallway = s.hallway.toMutableList()
+                                newHallway[hi] = c.value
+                                val newRooms = s.rooms.toMutableList()
+                                val newRoom = room.value.toMutableList()
+                                newRoom[c.index] = '.'
+                                newRooms[room.index] = newRoom
+                                queue.add(
+                                    StateCost(
+                                        State(
+                                            newHallway,
+                                            newRooms
+                                        ),
+                                        stateCost.cost + m * ((room.index + 1) + abs(hi - roomIndex[c.index]))
 
+                                    )
                                 )
-                            )
+                            }
                         }
                     }
                 }
@@ -119,7 +122,11 @@ fun main() {
                 newHallway[c.index] = '.'
                 if (newHallway.slice((c.index) toward roomIndex[index]).all { it == '.' }) {
                     for (room in s.rooms.withIndex()) {
-                        if ((0..room.index).all { s.rooms[it][index] == '.' }) {
+                        if (
+                            s.rooms[room.index][index] == '.'
+                            && (0..room.index).all { s.rooms[it][index] == '.' || s.rooms[it][index] == goals[index] }
+                            && (room.index == s.rooms.size - 1 || ((room.index + 1) until s.rooms.size).all { s.rooms[it][index] == goals[index] })
+                        ) {
                             val newRooms = s.rooms.toMutableList()
                             val newRoom = room.value.toMutableList()
                             newRoom[index] = c.value
